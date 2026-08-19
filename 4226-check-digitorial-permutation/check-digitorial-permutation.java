de0412aacae07e4c78438ff1295edcb4e5d6f2fd
Aadithya Vettrivel <aadithyavettrivel24@gmail.com>
2026-08-19 10:52:06 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (94.18%) - LeetSync

--- 4226-check-digitorial-permutation/check-digitorial-permutation.java
+++ 4226-check-digitorial-permutation/check-digitorial-permutation.java
@@ -1,28 +1,34 @@
 class Solution {
     public boolean isDigitorialPermutation(int n) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
         int fact = 1;
-        for(int i = 1; i <= 9; i++){
-            fact *= i;
-            list.add(fact);
+        int temp = n;
+        int sum = 0;
+        while(temp != 0){
+            sum += factorial(temp % 10);
+            temp /= 10;
         }
         int res = 0;
         int[] f1 = new int[10];
         while(n > 0){
             int n1 = n % 10;
             f1[n1]++;
-            res += list.get(n1);
             n /= 10;
         }
-        while(res > 0){
-            int n1 = res % 10;
+        while(sum > 0){
+            int n1 = sum % 10;
             f1[n1]--;
-            res /= 10;
+            sum /= 10;
         }
         for(int i : f1){
             if(i != 0) return false;
         }
         return true;
+    }
+    public int factorial(int num){
+        int fact = 1;
+        for(int i =1;i <= num;i++){
+            fact *= i;
+        }
+        return fact;
     }
 }
