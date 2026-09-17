f5fe8266012158cab96d851ecd7379f70dd16f8e
Aadithya Vettrivel <aadithyavettrivel24@gmail.com>
2026-09-17 10:03:56 +0530

    Time: 6 ms (30.22%) | Memory: 43.5 MB (44.13%) - LeetSync

--- 851-goat-latin/goat-latin.java
+++ 851-goat-latin/goat-latin.java
@@ -1,23 +1,20 @@
 class Solution {
     public String toGoatLatin(String sentence) {
         String[] words = sentence.split("\\s+");
+        String a = "maa";
+        StringBuilder res = new StringBuilder();
         for(int i = 0; i < words.length; i++){
             if("aeiouAEIOU".contains("" + words[i].charAt(0))){
-                words[i] += "ma";
+                words[i] += a;
             }
             else{
                 StringBuilder sb = new StringBuilder(words[i]);
                 char c = sb.charAt(0);
                 sb.deleteCharAt(0);
                 sb.append(c);
-                sb.append("ma");
+                sb.append(a);
                 words[i] = sb.toString();
             }
-        }
-        String a = "a";
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < words.length; i++){
-            words[i] += a;
             a += "a";
             res.append(words[i]);
             res.append(" ");
