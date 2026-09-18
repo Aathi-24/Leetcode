102edfef2174f103df1a9a03a1f2a3cdebca7faa
Aadithya Vettrivel <aadithyavettrivel24@gmail.com>
2026-09-18 19:09:01 +0530

    Time: 3 ms (99.02%) | Memory: 47.3 MB (42.03%) - LeetSync

--- 4313-password-strength/password-strength.java
+++ 4313-password-strength/password-strength.java
@@ -1,15 +1,23 @@
 class Solution {
-    public int passwordStrength(String p) {
-        int res = 0;
-        Set<Character> set = new HashSet<>();
-        for(char c : p.toCharArray()){
-            if(set.add(c)){
-                if(Character.isLowerCase(c)) res += 1;
-                else if(Character.isUpperCase(c)) res += 2;
-                else if(Character.isDigit(c)) res += 3;
-                else res += 5;
+    public int passwordStrength(String password) {
+        boolean[] seen = new boolean[128];
+        int total = 0;
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (seen[ch]) {
+                continue;
+            }
+            seen[ch] = true;
+            if (ch >= 'a' && ch <= 'z') {
+                total += 1;
+            } else if (ch >= 'A' && ch <= 'Z') {
+                total += 2;
+            } else if (ch >= '0' && ch <= '9') {
+                total += 3;
+            } else { 
+                total += 5;
             }
         }
-        return res;
+        return total;
     }
 }
